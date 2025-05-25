9c0bef6ca85529bb0b74e0533cb367a2ca9f02b4
Angelo Theodorou <encelo@gmail.com>
2025-05-25 19:31:54 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# nCine_14Years_Presentation
+
+A [Slidev](https://sli.dev/) presentation about my 14 years of developing [nCine](https://ncine.github.io/).
+
+Presented in Rome on June 5th, 2025 for the [/dev/games](https://devgames.org) game developing conference.
+
+## License
+
+Distributed under the Creative Commons Attribution-ShareAlike 4.0 International [license](https://creativecommons.org/licenses/by-sa/4.0/) (CC BY-SA 4.0).
---
+++ global-top.vue
@@ -0,0 +1,15 @@
+<!-- an example footer for pages -->
+<template>
+  <footer
+    v-if="$nav.currentLayout !== 'cover'"
+    class="absolute top-0 right-0 p-2"
+  >
+    {{ $nav.currentPage }} / {{ $nav.total }}
+  </footer>
+  <footer
+    v-if="$nav.currentLayout !== 'cover'"
+    class="absolute bottom-0 right-0 p-2"
+  >
+    <span class="text-xs"><a href="https://devgames.org/en/2025.html"><carbon-group-presentation /> /dev/games/2025</a></span>
+  </footer>
+</template>
